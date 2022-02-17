b89e3b1e933b278f0c08d3db79abb970bf9594eb
yajat10 <88446514+yajat10@users.noreply.github.com>
2022-02-17 17:02:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+song1="";
+song2="";
+function setup(){
+canvas=createCanvas(600,500)
+canvas.center()
+
+video=createCapture(VIDEO)
+video.hide()
+}
+function draw(){
+    image(video,0,0,600,500)
+}
+function preload(){
+    song1=loadSound("song1.mp3")
+    song2=loadSound("song2.mp3")
+}
